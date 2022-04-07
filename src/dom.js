4654056efcb508cd20728d7e854a52c095bc4cba
Ryan <ryanrana_4@hotmail.com>
2022-04-07 01:35:31 +0100

    finished functionality, need to finish UI

--- src/dom.js
+++ src/dom.js
@@ -3,8 +3,9 @@ const displayBoard = (playersboard, computersboard) => {
   let board = playersboard.getBoard();
   console.log(board);
 
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
+  for (let i = 9; i >= 0; i--) {
+    // decide whether like this or the 1 later, this one puts 1 at bottom other puts 1 at top
+    for (let j = 0; j < 10; j++) {
       let square2 = document.createElement("div");
       square2.textContent = board[i][j];
       square2.dataset.x = [j];
